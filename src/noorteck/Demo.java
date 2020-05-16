82f319ede25f4f56ee59188e3a6454a3692eff34
baburnaimi <baburnaimi@10.0.0.160>
2020-05-16 08:23:18 -0400

    my secound puch

--- src/noorteck/Demo.java
+++ src/noorteck/Demo.java
@@ -3,8 +3,8 @@ package noorteck;
 public class Demo {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 
+		System.out.println("GitHub will be easy thanks");
 	}
 
 }
